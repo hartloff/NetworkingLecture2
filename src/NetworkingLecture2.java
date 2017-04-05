7aadca3dbb4a03b31c4b57430b4051f137381782
jessehartloff <hartloff@buffalo.edu>
2017-04-04 20:46:43 -0400

    initial commit

---
+++ src/NetworkingLecture2.java
@@ -0,0 +1,18 @@
+
+public class NetworkingLecture2 {
+
+
+    // 5 points
+    static String Q1() {
+        // Send "lecture request" in the body of an HTTP POST request to http://fury.cse.buffalo.edu/api/lecture2
+        // and return the server's response
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
